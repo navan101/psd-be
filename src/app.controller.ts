0b92518ef341934a8bb3d6e086c303016cbde7b1
unknown <nguyenanhvan.net@gmail.com>
2020-07-24 18:09:35 +0700

    code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/psd')
+  async getPsd() {
+    return await this.appService.getPsd()
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import * as PSD from "psd";
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getPsd() {
+    let baseUrl = 'http://localhost:3000'
+    let ret = []
+    var psd = PSD.fromFile("src/assets/OBFJDK1.psd");
+    psd.parse();
+    // let tree = psd.tree().export().children
+    psd.image.saveAsPng('public/images/output.png');
+    let i = 0;
+    psd.tree().descendants().forEach(function (node) {
+      if (node.isGroup()) return true;
+      console.log(node)
+      node.saveAsPng("public/images/" + node.name + `_${i}` + ".png").catch(function (err) {
+        console.log(err.stack);
+      });
+      let res = {
+        top: node.layer.top,
+        left: node.layer.left,
+        bottom: node.layer.bottom,
+        right: node.layer.right,
+        channels: node.layer.channels,
+        height: node.layer.height,
+        rows: node.layer.rows,
+        width: node.layer.width,
+        cols: node.layer.cols,
+        opacity: node.layer.opacity,
+        visible: node.layer.visible,
+        clipped: node.layer.clipped,
+        layerEnd: node.layer.layerEnd,
+        image: `${baseUrl}/public/images/${node.name}_${i}.png`,
+      }
+      ret.push(res)
+
+      // node.imgBase64 = new Buffer(node.layer.file.data).toString('base64');
+      i++
+    });
+    return ret
   }
 }
